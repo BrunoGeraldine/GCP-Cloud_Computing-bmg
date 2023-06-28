ced565566a537e231df1d1a51c5febaeec6f40ef
BrunoGeraldine <databrunog@gmail.com>
2023-06-28 16:42:15 -0300

    all app

--- notebooks/app/app.py
+++ notebooks/app/app.py
@@ -1,6 +1,9 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
+import numpy          as np
+import pandas         as pd
+import streamlit      as st
+import plotly.express as px
+
+from PIL      import Image
 from datetime import date
 from plotly   import graph_objects as go
 
@@ -17,10 +20,7 @@ st.header('Análise das multas emitidas pela PRF - 2022')
 
 st.text('Este projeto visa demonstrar o potencial de visualização da análise de dados, utilizando o Streamlit')
 
-#criando a side bar
-st.sidebar.header('Escolha a UF do estado')
-
-#Coletando o nome e sigla da ação escolhida
+#Coleta dos dados
 @st.cache_data
 def pegar_dados_uf():
     path = '../dataset/uf_valor_total.parquet'
@@ -28,7 +28,6 @@ def pegar_dados_uf():
 
 df = pegar_dados_uf()
 
-#Coletando o nome e sigla da ação escolhida
 @st.cache_data
 def pegar_valores_uf():
     path = '../dataset/uf_valor_total.parquet'
@@ -36,30 +35,55 @@ def pegar_valores_uf():
 
 df1 = pegar_valores_uf()
 
+@st.cache_data
+def pegar_valores_mes():
+    path = '../dataset/anl_mes.parquet'
+    return pd.read_parquet(path)
+
+df_mes = pegar_valores_mes()
 
 
-uf = df['UF Infração']
-uf_escolhido = st.sidebar.selectbox('Escolha uma UF', uf)
+#criando a side bar
 
+uf = df1['UF Infração']
+sigla_uf_escolhida = st.sidebar.selectbox('Escolha a UF do estado', uf)
 
-st.subheader('Tabela de Valores - ' + uf_escolhido)
+df_uf = df1[df1['UF Infração'] == sigla_uf_escolhida]
+uf_escolhida = df_uf.iloc[0]['UF Infração']
+#
+#
+#
+#image = Image.open('logopf.webp')
+#st.image(image)
+#
+st.subheader('Tabela de Valores')
 st.write(df1.tail(28).sort_values(by='Qtdd', ascending=False))
 
-
+#
+##Criando grafico
+#st.subheader('Grafico - Valor Total Multas')
+#fig = go.Figure()
+#fig.add_trace(go.Bar(x=df1['UF Infração'],
+#                     y=df1['Total'],
+#                     name='Total em Multas po Estado'))
+#
+#fig.update_layout(width=1400, height=500, font_size=12, dragmode='zoom')
+#
+#st.plotly_chart(fig)
+#
 #Criando grafico
-st.subheader('Grafico de Multas')
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df1['UF Infração'],
-                         y=df1['Qtdd'],
+st.subheader('Grafico - Valor Total Multas P/ Mês')
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=df_mes['Mês'],
+                         y=df_mes['Total_mes'],
                          name='Quantidade de Multas po Estado',
-                         line_color='gray'))
-
-
-fig.add_trace(go.Scatter(x=df1['UF Infração'],
-                         y=df1['Total'],
-                         name='Total de Multas po Estado',
                          line_color='orange'))
 
-fig.update_layout(width=1400, height=500, font_size=12, dragmode='zoom')
+fig2.update_layout(width=1400, height=500, font_size=12, dragmode='zoom')
 
-st.plotly_chart(fig)
+st.plotly_chart(fig2)
+
+
+#fig2 = px.line(x=df_mes['Mês'], y=df_mes['Total_mes'])
+##fig2.write_html('first_figure.html', auto_open=True)
+#st.plotly_chart(fig2)
