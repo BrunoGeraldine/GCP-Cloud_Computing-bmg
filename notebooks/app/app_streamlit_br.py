ad52a1cfc651b5d33cfa78431edcce87427801b2
BrunoGeraldine <databrunog@gmail.com>
2023-06-29 10:38:34 -0300

    final streamlit app

--- notebooks/app/app_streamlit_br.py
+++ notebooks/app/app_streamlit_br.py
@@ -44,46 +44,51 @@ df_mes = pegar_valores_mes()
 
 
 #criando a side bar
-
+st.sidebar.image('./logopf.webp', width=180)
 uf = df1['UF Infração']
 sigla_uf_escolhida = st.sidebar.selectbox('Escolha a UF do estado', uf)
 
 df_uf = df1[df1['UF Infração'] == sigla_uf_escolhida]
 uf_escolhida = df_uf.iloc[0]['UF Infração']
+
+
+
+st.write(df_uf.iloc[0]['UF Infração'])
 #
-#
+##
 #
 #image = Image.open('logopf.webp')
 #st.image(image)
-#
-st.subheader('Tabela de Valores')
-st.write(df1.tail(28).sort_values(by='Qtdd', ascending=False))
 
+st.subheader('Tabela de Valores')
+st.write(df_uf.tail().sort_values(by='Qtdd', ascending=False))
 #
-##Criando grafico
-#st.subheader('Grafico - Valor Total Multas')
-#fig = go.Figure()
-#fig.add_trace(go.Bar(x=df1['UF Infração'],
-#                     y=df1['Total'],
-#                     name='Total em Multas po Estado'))
-#
-#fig.update_layout(width=1400, height=500, font_size=12, dragmode='zoom')
-#
-#st.plotly_chart(fig)
-#
+##
+###Criando grafico
+st.subheader('Grafico - Valor Total Multas')
+fig = go.Figure()
+fig.add_trace(go.Bar(x=df['UF Infração'],
+                     y=df['Total'],
+                     name='Total em Multas po Estado'
+                     ))
+
+fig.update_layout(width=1100, height=500, font_size=12, dragmode='zoom')
+
+st.plotly_chart(fig)
+##
 #Criando grafico
 st.subheader('Grafico - Valor Total Multas P/ Mês')
 fig2 = go.Figure()
-fig2.add_trace(go.Scatter(x=df_mes['Mês'],
-                         y=df_mes['Total_mes'],
-                         name='Quantidade de Multas po Estado',
-                         line_color='orange'))
+fig2.add_trace(go.Bar(x=df_mes['Mês'],
+                      y=df_mes['Total_mes'],
+                      name='Quantidade de Multas po Estado'
+                      ))
 
-fig2.update_layout(width=1400, height=500, font_size=12, dragmode='zoom')
+fig2.update_layout(width=1100, height=500, font_size=12, dragmode='zoom')
 
 st.plotly_chart(fig2)
-
-
-#fig2 = px.line(x=df_mes['Mês'], y=df_mes['Total_mes'])
-##fig2.write_html('first_figure.html', auto_open=True)
-#st.plotly_chart(fig2)
+#
+#
+##fig2 = px.line(x=df_mes['Mês'], y=df_mes['Total_mes'])
+###fig2.write_html('first_figure.html', auto_open=True)
+##st.plotly_chart(fig2)
